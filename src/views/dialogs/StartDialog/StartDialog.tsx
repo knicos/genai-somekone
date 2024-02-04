8e1e4e476320c31e5b3638a0001b9478587e9c86
Nicolas Pope <npope@uef.fi>
2024-02-04 17:23:02 +0000

    Merged PR 317: Restyle start dialog for consistency
    Restyle start dialog for consistency

--- src/views/dialogs/StartDialog/StartDialog.tsx
+++ src/views/dialogs/StartDialog/StartDialog.tsx
@@ -6,6 +6,7 @@ import { LargeButton } from '@genaism/components/Button/Button';
 import { useRecoilState } from 'recoil';
 import { menuShowShare } from '@genaism/state/menuState';
 import { useCallback } from 'react';
+import { Dialog, DialogContent, DialogTitle } from '@mui/material';
 
 interface Props {
     users: UserInfo[];
@@ -18,45 +19,52 @@ export default function StartDialog({ users, code }: Props) {
 
     const doClose = useCallback(() => setShowDialog(false), [setShowDialog]);
 
-    return showDialog ? (
-        <div className={style.groupedItems}>
-            <div className={style.connectMessage}>
-                <QRCode
-                    url={`${window.location.origin}/feed/${code}`}
-                    size="large"
-                />
-                <div className={style.column}>
-                    <div>
-                        <Trans
-                            values={{ codeText: code }}
-                            i18nKey="dashboard.messages.connection"
-                            components={{
-                                Code: <em />,
-                            }}
-                        />
+    return (
+        <Dialog
+            open={showDialog}
+            onClose={doClose}
+            maxWidth="md"
+        >
+            <DialogTitle className={style.title}>{t('dashboard.titles.connectUsers')}</DialogTitle>
+            <DialogContent>
+                <div className={style.connectMessage}>
+                    <QRCode
+                        url={`${window.location.origin}/feed/${code}`}
+                        size="large"
+                    />
+                    <div className={style.column}>
+                        <div style={{ textAlign: 'center' }}>
+                            <Trans
+                                values={{ codeText: code }}
+                                i18nKey="dashboard.messages.connection"
+                                components={{
+                                    Code: <em />,
+                                }}
+                            />
+                        </div>
+                        <a
+                            href={`${window.location.origin}`}
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            {window.location.host}
+                        </a>
                     </div>
-                    <a
-                        href={`${window.location.origin}`}
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        {window.location.host}
-                    </a>
                 </div>
-            </div>
-            <div className={style.userListing}>
-                {users.length === 0 && <div>{t('dashboard.messages.waitingPeople')}</div>}
-                {users.length === 1 && <div>{t('dashboard.messages.onePerson', { count: users.length })}</div>}
-                {users.length > 1 && <div>{t('dashboard.messages.manyPeople', { count: users.length })}</div>}
-                <LargeButton
-                    variant="contained"
-                    color="secondary"
-                    data-testid="dashboard-start-button"
-                    onClick={doClose}
-                >
-                    {t('dashboard.actions.start')}
-                </LargeButton>
-            </div>
-        </div>
-    ) : null;
+                <div className={style.userListing}>
+                    {users.length === 0 && <div>{t('dashboard.messages.waitingPeople')}</div>}
+                    {users.length === 1 && <div>{t('dashboard.messages.onePerson', { count: users.length })}</div>}
+                    {users.length > 1 && <div>{t('dashboard.messages.manyPeople', { count: users.length })}</div>}
+                    <LargeButton
+                        variant="contained"
+                        color="secondary"
+                        data-testid="dashboard-start-button"
+                        onClick={doClose}
+                    >
+                        {t('dashboard.actions.start')}
+                    </LargeButton>
+                </div>
+            </DialogContent>
+        </Dialog>
+    );
 }
