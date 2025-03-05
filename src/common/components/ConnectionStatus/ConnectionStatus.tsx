240c08548bdb0f35bd9919df7148816dc87f833e
Nicolas Pope <npope@uef.fi>
2025-03-05 05:16:02 +0000

    Merged PR 731: Fix connection status style problem
    Fix connection status style problem
    
    Related work items: #1113

--- src/common/components/ConnectionStatus/ConnectionStatus.tsx
+++ src/common/components/ConnectionStatus/ConnectionStatus.tsx
@@ -1,6 +1,7 @@
 import { ConnectionStatus as BaseConnectionStatus, Peer2Peer } from '@knicos/genai-base';
 import style from './style.module.css';
 import { EventProtocol } from '@genaism/protocol/protocol';
+import { useEffect, useState } from 'react';
 
 interface Props {
     position: 'corner' | 'center';
@@ -10,7 +11,22 @@ interface Props {
 }
 
 export default function ConnectionStatus({ peer, ready, position, visibility }: Props) {
-    return (
+    const [quality, setQuality] = useState(0);
+
+    useEffect(() => {
+        if (peer) {
+            peer.on('quality', setQuality);
+            setQuality(peer.quality);
+
+            return () => {
+                peer.off('quality', setQuality);
+            };
+        } else {
+            setQuality(0);
+        }
+    }, [peer]);
+
+    return visibility === undefined || quality <= visibility ? (
         <div className={position === 'corner' ? style.corner : style.center}>
             <BaseConnectionStatus
                 api={import.meta.env.VITE_APP_APIURL}
@@ -20,5 +36,5 @@ export default function ConnectionStatus({ peer, ready, position, visibility }: 
                 visibility={visibility}
             />
         </div>
-    );
+    ) : null;
 }
